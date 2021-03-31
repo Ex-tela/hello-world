83f064a3a7614d6dcc6b1a7e9a3f04b386a32daf
Ex-tela <81362694+Ex-tela@users.noreply.github.com>
2021-03-31 14:08:16 +0100

    A new greeting to add to hello world

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,12 @@
 print('Hello World!')
+
+user_hi_amount = input('insert number here: ')
+
+def greeter(hi_amount):
+    try:
+        response = 'Hi' * int(hi_amount)
+    except ValueError:
+        response = 'This is not an usable integer.'
+    return response
+
+print (greeter(user_hi_amount))
