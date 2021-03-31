875e5657de66d1bf3bda9a8a8f51a439a5ba719d
Ex-tela <81362694+Ex-tela@users.noreply.github.com>
2021-03-31 14:59:09 +0100

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,12 +1,14 @@
-print('Hello World!')
-
-user_hi_amount = input('insert number here: ')
-
 def greeter(hi_amount):
     try:
-        response = 'Hi' * int(hi_amount)
+        response = 'Hi ' * int(hi_amount)
     except ValueError:
         response = 'This is not an usable integer.'
     return response
 
-print (greeter(user_hi_amount))
+def main():
+   print('Hello World!')
+   user_hi_amount = input('insert number here: ')
+   print(greeter(user_hi_amount))
+
+if __name__ == "__main__":
+   main()
